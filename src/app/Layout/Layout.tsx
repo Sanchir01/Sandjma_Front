8cb2861ebc545d179e6e3ede1920c95aaaa2fbd4
Anton <anstonebear@mail.ru>
2023-12-03 19:51:45 +0300

    footer__mobile

--- src/app/Layout/Layout.tsx
+++ src/app/Layout/Layout.tsx
@@ -1,11 +1,12 @@
 import { Header } from '@/widgets/header'
 import { FC, ReactNode } from 'react'
+import { Footer } from '@/widgets/footer'
 export const Layout: FC<{ children?: ReactNode }> = ({ children }) => {
 	return (
 		<>
 			<Header />
 			{children}
-			{/* <Footer /> */}
+			<Footer />
 		</>
 	)
 }
