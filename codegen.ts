0002bcbe796dd5d9b9f69b7a6e5d265b338de10d
Sanchir01 <emgushovs@mail.ru>
2024-02-05 12:27:13 +0300

    mini update

--- codegen.ts
+++ codegen.ts
@@ -2,7 +2,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli'
 
 const config: CodegenConfig = {
 	overwrite: true,
-	schema: 'https://sandjmaback-production.up.railway.app/graphql',
+	schema: 'https://sandjmaback.ru/graphql',
 	documents: 'graphql/mySchemas/**/*.gql',
 	generates: {
 		'graphql/gql/': {
