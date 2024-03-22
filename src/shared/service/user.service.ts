708323cbd28d7e6566510e63738df62e2bd0ce21
Sanchir01 <emgushovs@mail.ru>
2024-03-22 15:18:33 +0300

    update request graphql

--- src/shared/service/user.service.ts
+++ src/shared/service/user.service.ts
@@ -1,9 +1,20 @@
 import { GetUserProfileDocument } from 'gql/gql/graphql'
 import { GraphQLClient } from 'graphql-request'
+import { AuthServiceTokens } from '../utils/Tokens.service'
 
-export const myRequest = new GraphQLClient(process.env.SERVER_URL ?? '', {
-	credentials: 'include'
-})
+// const refreshToken = AuthServiceTokens.getAccessToken()
+export const myRequest = new GraphQLClient(
+	process.env.NODE_ENV === 'development'
+		? 'http://localhost:5000'
+		: process.env.SERVER_URL ?? '',
+	{
+		credentials: 'include',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		mode: 'cors'
+	}
+)
 
 export const userService = {
 	async getProfile() {
