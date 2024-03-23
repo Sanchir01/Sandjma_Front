ad304de5df335f2ee8ecc556e2b179531f06950f
Sanchir01 <emgushovs@mail.ru>
2024-03-23 11:37:10 +0300

    update headers

--- src/shared/service/user.service.ts
+++ src/shared/service/user.service.ts
@@ -1,9 +1,12 @@
 import { GetUserProfileDocument } from 'gql/gql/graphql'
 import { GraphQLClient } from 'graphql-request'
-
+import { AuthServiceTokens } from '../utils/Tokens.service'
+const refreshToken = AuthServiceTokens.getRefreshToken()
 export const myRequest = new GraphQLClient(process.env.SERVER_URL ?? '', {
 	credentials: 'include',
-
+	headers: {
+		Cookie: refreshToken ? `refreshToken=${refreshToken}` : ''
+	},
 	mode: 'cors'
 })
 
