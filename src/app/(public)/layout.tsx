87720e44294e0895e0557a4371850b13a1c89a33
Sanchir01 <emgushovs@mail.ru>
2024-03-23 11:25:42 +0300

    update

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -10,7 +10,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 	const updateUser = useUser(state => state.resetUser)
 	const setUser = useUser(state => state.setUser)
 	const { data } = useUserProfile()
-	
+
 	useEffect(() => {
 		if (refreshToken == undefined) {
 			updateUser()
