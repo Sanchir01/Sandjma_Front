a77fee7f9393f919d7ecb1d9b2ca73c78da26835
Sanchir01 <emgushovs@mail.ru>
2024-03-31 22:29:55 +0300

    update codegen

--- codegen.ts
+++ codegen.ts
@@ -2,10 +2,8 @@ import type { CodegenConfig } from '@graphql-codegen/cli'
 
 const config: CodegenConfig = {
 	overwrite: true,
-	schema:
-		process.env.NODE_ENV === 'production'
-			? process.env.SERVER_GRAPHQL
-			: `http://localhost:5000/graphql`,
+	schema: process.env.SERVER_URL as string,
+
 	documents: 'graphql/mySchemas/**/*.gql',
 	generates: {
 		'graphql/gql/': {
