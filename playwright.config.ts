b19889a91273a06fc37cccf29feac7c8d2c32b53
Sanchir01 <emgushovs@mail.ru>
2024-04-03 23:44:35 +0300

    create deploy cd and playwright tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
-		baseURL: process.env.TEST_ENV_BASE_TEST,
+		baseURL: process.env.NEXT_PUBLIC_CLIENT_URL,
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: 'on-first-retry'
@@ -38,11 +38,6 @@ export default defineConfig({
 			use: { ...devices['Desktop Firefox'] }
 		},
 
-		{
-			name: 'webkit',
-			use: { ...devices['Desktop Safari'] }
-		}
-
 		/* Test against mobile viewports. */
 		// {
 		//   name: 'Mobile Chrome',
@@ -54,14 +49,14 @@ export default defineConfig({
 		// },
 
 		/* Test against branded browsers. */
-		// {
-		//   name: 'Microsoft Edge',
-		//   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-		// },
-		// {
-		//   name: 'Google Chrome',
-		//   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-		// },
+		{
+			name: 'Microsoft Edge',
+			use: { ...devices['Desktop Edge'], channel: 'msedge' }
+		},
+		{
+			name: 'Google Chrome',
+			use: { ...devices['Desktop Chrome'], channel: 'chrome' }
+		}
 	]
 
 	/* Run your local dev server before starting the tests */
--- src/features/themesToggle/toggleTheme.spec.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react'
-import { ToggleTheme } from './toggle-theme'
-
-describe('Toggle theme', () => {
-	it('Toggle', async () => {
-		render(<ToggleTheme />)
-	})
-})
